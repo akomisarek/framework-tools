40382981ce79f62feb2f1b1aa0f44bcd3f98b7d5
Shaun Francis <Shaun.Francis@hmcts.net>
2016-11-03 15:46:38 +0000

    Simplify command setup and refactor Bootstrap

--- framework-tools-command/src/main/java/uk/gov/justice/framework/tools/common/command/ShellCommand.java
+++ framework-tools-command/src/main/java/uk/gov/justice/framework/tools/common/command/ShellCommand.java
@@ -2,11 +2,11 @@ package uk.gov.justice.framework.tools.common.command;
 
 /**
  * Implement this interface to have the reflection utils pick
- * up the class at runtime and add to Jcommander instance in order
+ * up the class at runtime and add to JCommander instance in order
  * to setup a CMD command.
  */
 public interface ShellCommand {
 
-    void run(String[] args);
+    void run(final String[] args);
 
 }
