ff23189712c95094ab7eb2c53f60ba70aae200e9
Shaun Francis <Shaun.Francis@hmcts.net>
2016-11-15 16:17:08 +0000

    Add CLI database cleaning to shell script

--- framework-tools-replay/start.sh
+++ framework-tools-replay/start.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
+
+# Drop tables
+java -jar notification-viewstore-liquibase-1.0.12-SNAPSHOT.jar --url=jdbc:postgresql://localhost:5432/notificationviewstore --username=notification --password=notification --logLevel=info dropAll
+
+# Update view store tables
+java -jar notification-viewstore-liquibase-1.0.12-SNAPSHOT.jar --url=jdbc:postgresql://localhost:5432/notificationviewstore --username=notification --password=notification --logLevel=info update
+
+# Update event buffer tables
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy -DoutputDirectory=target -Dartifact=uk.gov.justice.services:event-buffer-liquibase:0.33.0:jar
+java -jar target/event-buffer-liquibase-0.33.0.jar --url=jdbc:postgresql://localhost:5432/notificationviewstore --username=notification --password=notification --logLevel=info update
+
+# Replay event streams
 java -jar target/framework-tools-replay-1.0.0-SNAPSHOT-swarm.jar replay -c standalone-ds.xml -l notification-event-listener-1.0.12-SNAPSHOT.war
